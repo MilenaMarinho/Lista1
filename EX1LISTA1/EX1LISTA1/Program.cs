3fb62e151cb4bfc0f843dcb028b3c732b6dc26c1
MilenaMarinho <130614123+MilenaMarinho@users.noreply.github.com>
2023-08-17 20:26:51 -0300

    Add files via upload

---
+++ EX1LISTA1/EX1LISTA1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1LISTA1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            inverteTela();
+            Retangulo r;
+            r = new Retangulo();
+            Console.Write("Informe a altura: ");
+            r.setAltura(double.Parse(Console.ReadLine()));
+            Console.Write("Informe a base: ");
+            r.setBase(double.Parse(Console.ReadLine()));
+            r.calcularArea();
+            Console.WriteLine(" A área do retângulo é: {0}",
+                r.getArea());
+        }
+        static void inverteTela()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+        }
+
+    }
+
+}
+  
