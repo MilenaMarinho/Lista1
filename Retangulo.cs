b9aaddbf3dbc3d125fbaefa9a970286b596b2965
MilenaMarinho <130614123+MilenaMarinho@users.noreply.github.com>
2023-08-17 20:21:09 -0300

    Add files via upload

---
+++ Retangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1LISTA1
+{
+    internal class Retangulo
+    {
+        #region Atributos
+        private double altura;
+        private double basee;
+        private double area;
+        #endregion
+
+        #region Metodos Interface
+        public void setAltura (double p)
+        {
+            altura = p;
+        }
+        public void setBase(double p)
+        {
+            basee = p;
+        }
+        public double getAltura()
+        {
+            return altura;
+        }
+        public double getBase()
+        {
+            return basee;
+        }
+        public double getArea()
+        {
+            return area;
+        }
+        #endregion
+        #region MetodosFuncionais
+        public void calcularArea()
+        {
+            area = altura * basee;
+        }
+        #endregion
+    }
+}
